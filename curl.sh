40a93327583921ee461840b99f6b71052d7e9c2a
rdago <info@rdago.de>
2020-05-01 10:20:40 +0200

    bug fixes

--- curl.sh
+++ curl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-while read x 
-do 
-    RESPONSE=curl -s -k -o /dev/null -w "%{http_code}" $x
-    if [[ $RESPONSE -eq 200 ]]
+while read x
+do
+    RESPONSE=$(curl -s -k -o /dev/null -w "%{http_code}" $x)
+    if [ $RESPONSE -eq 200 ]
     then
-        echo -e "\e[32m[+] Object created.\e[0m" 
-    else 
+        echo -e "\e[32m[+] Object created.\e[0m"
+    else
         echo -e "\e[31m[-] An Error occured Processing this Item.\e[0m"
     fi
 done
