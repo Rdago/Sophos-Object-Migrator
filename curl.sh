f2d87abbcd84675e379b8c0f9f4da4e1a7fc0e41
Rdago <62183866+Rdago@users.noreply.github.com>
2020-05-01 09:59:43 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -2,5 +2,11 @@
 
 while read x 
 do 
-    curl -k -I $x
+    RESPONSE=curl -s -k -o /dev/null -w "%{http_code}" $x
+    if [[ $RESPONSE -eq 200 ]]
+    then
+        echo -e "\e[32m[+] Object created.\e[0m" 
+    else 
+        echo -e "\e[31m[-] An Error occured Processing this Item.\e[0m"
+    fi
 done
